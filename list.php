0115aae2f2aab7a4494c3564f63738bf159780af
Adam K Dean <adamkdean@googlemail.com>
2013-07-10 11:09:47 +0100

    Added JSON list PHP file

---
+++ list.php
@@ -0,0 +1,28 @@
+<?php
+
+/* GalleryJS (https://github.com/Imdsm/GalleryJS)
+ * Adam K Dean (Imdsm)
+ * 08/07/2013 16:32
+ *
+ * After much deliberation, I decided that PHP was the easiest, 
+ * and most accessible way to quickly print a sorted JSON list.
+ */
+
+function list_images($path) {
+	$list = array();
+	if ($dir = opendir($path)) {		
+		while (($file = readdir($dir)) !== false) {
+			if ($file != "." && $file != "..") {				
+				$list[$path . $file] = filemtime($path . $file);
+			}
+		}
+		closedir($dir);
+		arsort($list, SORT_NATURAL);
+	}
+	return $list;
+}
+
+$list = list_images("img/");
+echo json_encode($list);
+
+?>
